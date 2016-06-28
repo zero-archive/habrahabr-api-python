21f9f490a242bd697f48e4107c3fab0ab3d58a1e
dotzero <mail@dotzero.ru>
2016-06-28 10:51:44 +0300

    pep257

--- habrahabr/api.py
+++ habrahabr/api.py
@@ -19,36 +19,135 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+"""Этот модуль содержит клиент для получения доступа к ресурсам API."""
 
-# from .comments import CommentsResource
-# from .company import CompanyResource
-# from .feed import FeedResource
-# from .flow import FlowResource
-# from .hub import HubResource
-# from .poll import PollResource
-# from .post import PostResource
-# from .search import SearchResource
-# from .settings import SettingsResource
-# from .tracker import TrackerResource
-# from .user import UserResource
-from .resources import *
-from .errors import ApiHandlerError
+from .resources.comments import CommentsResource
+from .resources.company import CompanyResource
+from .resources.feed import FeedResource
+from .resources.flow import FlowResource
+from .resources.hub import HubResource
+from .resources.poll import PollResource
+from .resources.post import PostResource
+from .resources.search import SearchResource
+from .resources.settings import SettingsResource
+from .resources.tracker import TrackerResource
+from .resources.user import UserResource
+from habrahabr.errors import ApiHandlerError
+from habrahabr.utils import lazy
 
 
 class Api(object):
+    """Этот объект содержит клиент для получения доступа к ресурсам API.
+
+    Args:
+        auth (object): Экземпляр класса Auth.
+    """
+
     def __init__(self, auth=None):
+        """Конструктор.
+
+        :param auth: Экземпляр класса Auth.
+        :rtype: object
+        """
         if auth is None:
             raise ApiHandlerError('Auth handler is not defined')
-
         self.auth = auth
-        self.comments = CommentsResource(self.auth)
-        self.company = CompanyResource(self.auth)
-        self.feed = FeedResource(self.auth)
-        self.flow = FlowResource(self.auth)
-        self.hub = HubResource(self.auth)
-        self.poll = PollResource(self.auth)
-        self.post = PostResource(self.auth)
-        self.search = SearchResource(self.auth)
-        self.settings = SettingsResource(self.auth)
-        self.tracker = TrackerResource(self.auth)
-        self.user = UserResource(self.auth)
+
+    @lazy
+    def comments(self):
+        """Ресурс работы с комментариями.
+
+        :returns: CommentsResource.
+        :rtype: object
+        """
+        return CommentsResource(self.auth)
+
+    @lazy
+    def company(self):
+        """Ресурс работы с компаниями.
+
+        :returns: CompanyResource.
+        :rtype: object
+        """
+        return CompanyResource(self.auth)
+
+    @lazy
+    def feed(self):
+        """Ресурс работы с основной лентой постов.
+
+        :returns: FeedResource.
+        :rtype: object
+        """
+        return FeedResource(self.auth)
+
+    @lazy
+    def flow(self):
+        """Ресурс работы с потоками.
+
+        :returns: FlowResource.
+        :rtype: object
+        """
+        return FlowResource(self.auth)
+
+    @lazy
+    def hub(self):
+        """Ресурс работы с хабами.
+
+        :returns: HubResource.
+        :rtype: object
+        """
+        return HubResource(self.auth)
+
+    @lazy
+    def poll(self):
+        """Ресурс работы с опросами.
+
+        :returns: PollResource.
+        :rtype: object
+        """
+        return PollResource(self.auth)
+
+    @lazy
+    def post(self):
+        """Ресурс работы с постами.
+
+        :returns: PostResource.
+        :rtype: object
+        """
+        return PostResource(self.auth)
+
+    @lazy
+    def search(self):
+        """Ресурс работы с поиском.
+
+        :returns: SearchResource.
+        :rtype: object
+        """
+        return SearchResource(self.auth)
+
+    @lazy
+    def settings(self):
+        """Ресурс работы с настройками профиля.
+
+        :returns: SettingsResource.
+        :rtype: object
+        """
+        return SettingsResource(self.auth)
+
+    @lazy
+    def tracker(self):
+        """Ресурс работы с трекером.
+
+        :returns: TrackerResource.
+        :rtype: object
+        """
+        return TrackerResource(self.auth)
+
+    @lazy
+    def user(self):
+        """Ресурс работы с пользователями.
+
+        :returns: UserResource.
+        :rtype: object
+        """
+        return UserResource(self.auth)
--- habrahabr/utils.py
+++ habrahabr/utils.py
@@ -40,3 +40,17 @@ def accepts(*types):
         return wrapped
 
     return decorated
+
+
+def lazy(func):
+    @property
+    def wrapper(self):
+        attr_name = '__%s' % func.__name__
+        try:
+            value = getattr(self, attr_name)
+        except AttributeError:
+            value = func(self)
+            setattr(self, attr_name, value)
+        return value
+
+    return wrapper
